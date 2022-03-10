41e57367ff51fea86e86b3d6d63420139de246b2
Tyler <tg0046@mix.wvu.edu>
2022-03-10 12:53:59 -0500

    began course page

--- CS230_Group12/src/app/app-routing.module.ts
+++ CS230_Group12/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourseHeaderComponent } from './Layouts/Course Page/Header/courseHeader.component';
 import { HomePageLayout } from './Layouts/Home Page/home-page-layout.component';
 
 const routes: Routes = [
   { path: '', component: HomePageLayout },
   { path: 'Home', component: HomePageLayout },
+  { path: 'Course', component: CourseHeaderComponent}
 ];
 
 @NgModule({
